d5818d1a2d95b882503f8d0f3ad0ae79b34d125e
Luca <luca.pourceau@gmail.com>
2024-03-03 00:21:26 +0100

    refractor: rename the exemple class to more explicit name

--- src/main/java/fr/izeleam/utils/kcommand/exemples/PluginCommand.java
+++ src/main/java/fr/izeleam/utils/kcommand/exemples/PluginCommand.java
@@ -10,7 +10,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class PluginCommand extends JavaPlugin {
 
     @Override
     public void onEnable() {
